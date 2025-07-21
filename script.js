3fb94605402d2ae59337987bf5e7acd1099deaa0
sicxx-xnx <hpeyton33@gmail.com>
2025-07-20 20:39:39 -0500

    add to cart feature
    enabled add to cart. however the add to cart increment is not working. needs to be debugged. when uncommeneted it breaks everything and wont add the product at all

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+allItems = [
+{id: 1,name: "something", price: 65, itemcount: 0, }
+]
+cartItems = []
+matchitem = []
 const indexBody = document.querySelector("body")
 const cartMainBody = document.createElement("div")
 const cartInnerBodyArr = [document.createElement("div"),document.createElement("div"),document.createElement("div")]
+const cartItemContainer = document.createElement("ul")
 const introHTML = document.querySelector(".intro")
 const introImage = document.querySelector(".intro-image")
 const checkoutButton = document.querySelector(".checkout")
 let total ;
 const cartInnerBodyArr_header = document.createElement("h2")
 const cartInnerBodyArr_CartIsEmptyHTML = document.createElement("p")
-cartInnerBodyArr_CartIsEmptyHTML.innerText = "Your cart is Empty"
+// cartInnerBodyArr_CartIsEmptyHTML.innerText = "Your cart is Empty"
+let cartItemPlaceholder = document.createElement("div")
 
 
 checkoutButton.addEventListener("click",openCart)
@@ -38,6 +45,10 @@ cartMainBody.style.borderRadius = "25% 10%"
 
 // indexBody.style.position = "absolute"
 
+
+cartItemContainer 
+
+
 //function - open/closecart
 
 function openCart(){
@@ -48,17 +59,44 @@ cartMainBody.appendChild(cartInnerBodyArr[1])
 cartMainBody.appendChild(cartInnerBodyArr[2])
 cartInnerBodyArr[0].appendChild(cartInnerBodyArr_header)
 cartInnerBodyArr[1].appendChild(cartInnerBodyArr_CartIsEmptyHTML)
-
-if (cartInnerBodyArr[1].innerHTML.trim() == "") {
-cartInnerBodyArr[1].innerText =  "Your cart is Empty"  
+cartInnerBodyArr[1].appendChild(cartItemContainer)
+if (cartItemContainer.innerHTML.trim() == "") {
+cartItemContainer.innerText =  "Your cart is Empty"  
+}
 }
 
 
+// allItems = [
+// {id: 1,name: "something", price: 65, itemcount: 0, }
+// ]
+// cartItems = []
+// matchitem = []
+function addtoCart(productid) {
+// let cartindex = cartItems.findindex((item) => item.id === productid)
+// if (cartindex !== -1) {
+// cartItems[cartindex].itemcount++    
+// } else {
+let matchitems = allItems.find((x) => x.id === productid)
+cartItems.push(matchitems)
+cartItems.forEach((item) => {
+    li = document.createElement("div");
+
+    const nameElement = document.createElement("p");
+    nameElement.innerText = `Name: ${item.name}`;
+
+    const priceElement = document.createElement("p");
+    priceElement.innerText = `Price: $${item.price}`;
+    cartItemContainer.appendChild(li)
+    li.appendChild(nameElement)
+    li.appendChild(priceElement)
+  
+})}
+
+addtoCart(1)
 
 
 
 
 
 
-}
 
